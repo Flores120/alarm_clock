ec6cc344fe9978ac6487482098f0bdb0753ac52d
Alvaro Flores <alvaro.flores01@gmail.com>
2017-03-30 18:19:55 -0700

    adds clock interface

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ var time = new Date()
 var h = time.getHours() % 12 || 12;
 var hh = h.toString();
 var m = time.getMinutes().toString();
-if (m < 10) {
-  m = "0" + m;
-}
 var s = time.getSeconds().toString();
 
-
+function checkTime() {
+  if (m < 10) {
+    m = "0" + m;
+  } if (s < 10) {
+    s = "0" + s;
+  }
+}
+checkTime();
 document.getElementById("time").innerHTML = hh + ":" + m + ":" + s;
