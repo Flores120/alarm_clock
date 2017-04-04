34355e6890618bff11f68b9e87d8bd11c9288153
Alvaro Flores <alvaro.flores01@gmail.com>
2017-04-04 16:23:03 -0700

    convert string time into a number

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,32 @@ var setTime = function() {
   var time = document.getElementById("timeSet").value;
   var slice1 = time.substring(0,2);
   var slice2 = time.substring(3,5);
-
-
+  var secondJoin = slice1 + slice2;
+  var convert = parseInt(secondJoin);
   if (slice1 >=  12) {
     slice2 = slice2 + " PM";
   } else {
     slice2 = slice2 + " AM";
   }
+  console.log(convert);
   slice1 = slice1 % 12 || 12;
   var joinAr = slice1 + ":" + slice2;
 
   var setTo = joinAr.toString();
+  console.log(setTo);
   document.getElementById("showTimeSet").innerHTML = setTo;
-  var getNumber = parseInt(slice1, slice2);
-  var add = slice1 + slice2;
-  console.log(add);
 }
+
 
 
 var startTime = function () {
 var time = new Date()
 var h = time.getHours();
-var m = time.getMinutes();
-console.log(m);
+var m = time.getMinutes().toString()
+
+var newT = h + m;
+var convert = parseInt(newT);
+console.log(convert);
 if (m < 10 ) {
   m = "0" + m;
 }
@@ -34,12 +37,11 @@ if (h > 12) {
   m = m + " AM"
 }
 h = h % 12 || 12;
-time = h + ":" + m;
-document.getElementById("time").innerHTML = time.toString();
+var time = h + ":" + m;
+var timeTo= time.toString();
+var getNum = timeTo.valueOf();
+document.getElementById("time").innerHTML = timeTo;
 var t = setTimeout(startTime, 500);
-time = h + m;
-var getNumber = parseInt(time);
-console.log(getNumber);
 }
 
 function checkTime(i) {
@@ -47,13 +49,3 @@ function checkTime(i) {
     return i;
 }
 checkTime();
-
-// var time1 = setTime(time);
-// var time2 = startTime(time);
-
-function compare (time1, time2){
-  if (time1 == time2) {
-    alert("times up");
-  }
-}
-compare();
